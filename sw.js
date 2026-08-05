a648c39a2f3a1f85f462787ba69d7ee58419318f
misschevious-agtk <tara.harris@prosus.com>
2026-08-05 22:57:10 +0200

    fix: refresh PWA cache for allowance tracker

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE = 'florida-2026-v9';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'florida-2026-v10';
+const FILES = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
+self.addEventListener('activate', event => {
+  event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
